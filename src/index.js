66dedcf2ad2f944a93fa4ea7abe6203714e5741e
AKrotchko <47993343+AKrotchko@users.noreply.github.com>
2021-10-25 00:40:49 -0400

    Fixed gitignore

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES] });
 
+const config = require("./config.js");
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -9,11 +11,11 @@ client.on('ready', () => {
 client.on('messageCreate', msg => {
     if (msg.author.bot) return;
 
-        // if (!msg.content.startsWith(settings.prefix)) return;
+    if (!msg.content.startsWith(config.prefix)) return;
 
-if (msg.content.startsWith('ping')) {
-    msg.reply('Pong!');
-}
+    if (msg.content.startsWith(`${config.prefix}ping`)) {
+        msg.reply('Pong!');
+    }
 });
 
-client.login('');
+client.login(config.token);
