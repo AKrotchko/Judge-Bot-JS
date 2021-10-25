e7b9a927f055a387714899d54a3bd4489a460d4e
AKrotchko <47993343+AKrotchko@users.noreply.github.com>
2021-10-25 01:02:08 -0400

    Updated config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES] });
 
-const config = require("./config.js");
+const config = require('./config/config.json');
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -9,13 +9,14 @@ client.on('ready', () => {
 
 
 client.on('messageCreate', msg => {
+    let prefix = '$';
     if (msg.author.bot) return;
 
     if (!msg.content.startsWith(config.prefix)) return;
 
-    if (msg.content.startsWith(`${config.prefix}ping`)) {
+    if (msg.content.startsWith(`${prefix}ping`)) {
         msg.reply('Pong!');
     }
 });
 
-client.login(config.token);
+client.login(`${config.token}`);
